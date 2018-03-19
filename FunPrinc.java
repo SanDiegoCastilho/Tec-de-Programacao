17312b0c36e3c5dbfbb530bd629ce72118384f07
Breno Araújo <31549095+brenoaraujolima@users.noreply.github.com>
2018-03-18 21:06:45 -0300

    Update FunPrinc.java
    Troca da variável "tamanho" por "elementos.length".

--- FunPrinc.java
+++ FunPrinc.java
@@ -1,8 +1,7 @@
-//San Diego - 10/03/2018 - 21:26.
-//Função principal - testes da classe Conjunto.
+public class FunPrinc {
 
-public class FunPrinc{
-	public static void main(String[] args){
+	public static void main(String[] args) {
+
 		Conjunto CInteiros = new Conjunto(10); //dez primeiros números naturais.
 		Conjunto CPares = new Conjunto(5); //cinco primeiros números pares.
 		Conjunto CImpares = new Conjunto(5); //cinco primeiros números ímpares.
@@ -97,7 +96,7 @@ public class FunPrinc{
 		Conjunto ConjUni = CPares.union(CImpares);
 		boolean  aux = true; 
 
-		for (int i = 0; i < ConjUni.tamanho; i++) {
+		for (int i = 0; i < ConjUni.elementos.length; i++) {
 			if (!CInteiros.checkExistence(ConjUni.elementos[i])) {
 				aux = false;
 			}
@@ -114,7 +113,7 @@ public class FunPrinc{
 		// D - Intersecção dos conjuntos CInteiros e CPares é vazia.
 		Conjunto ConjInt = CInteiros.inter(CPares);
 
-		if (ConjInt.tamanho > 0) {
+		if (ConjInt.elementos.length > 0) {
 			System.out.println("CInteiros intersecção CPares != Vazio");
 
 		}else{
@@ -127,9 +126,22 @@ public class FunPrinc{
 		Conjunto ConDif = CInteiros.dif(CPares);
 
 		System.out.println("Elementos do conjunto diferença de CInteiros e CPares");
-		for (int i = 0; i < ConDif.tamanho; i++) {
+		for (int i = 0; i < ConDif.elementos.length; i++) {
 			System.out.println(ConDif.elementos[i]);
 		}
 
+		//Produto Cartesiano Teste1 x Teste2.
+
+		Conjunto Teste1 = new Conjunto(4);
+		Conjunto Teste2 = new Conjunto(2);
+
+		Teste1.setElement(1);
+		Teste1.setElement(2);
+		Teste1.setElement(3);
+		Teste1.setElement(4);
+		Teste2.setElement(5);
+		Teste2.setElement(6);
+		
 	}
+	
 }
