351738766ac48d63b36e6295658218c5c02200e6
SanDiegoCastilho <sandiegocastilho@gmail.com>
2018-03-27 21:55:44 -0300

    Modificações da classe figura e da classe quadrado

--- Figura/Figura.java
+++ Figura/Figura.java
@@ -1,21 +1,11 @@
-abstract class Figura{
-
-	//Origem da imagem.
-	int origemX;
-	int origemY;
-
-	//Atirbutos de largura e altura.
-	int dx;
-	int dy;
-
-	//Construtor da classe Figura.
+public abstract class Figura{
 
 	//Desenha a figura na tela.
-	void desenhar();
+	public abstract void desenha();
 
 	//Mover a figura da tela.
-	void mover(float dx, float dy);
+	public abstract void mover(float dx, float dy);
 
 	//Calcula a área da figura.
-	double calculaArea();
+	public abstract double calculaArea();
 }
--- Figura/Quadrado.java
+++ Figura/Quadrado.java
@@ -1,40 +1,54 @@
-class Quadrado extends Figura{
+public class Quadrado extends Figura{
 
-	int origemX;
-	int origemY;
-
-	int dx;
-	int dy;
-
-	int largura;
-	int altura;
+	private double Dx;
+	private double Dy;
 
 	//Construtor da classe Quadrado.
-	Quadrado(int origemX, int origemY, int dx, int dy){
+	Quadrado(double dx, double dy){
 		if (dx == dy) { //Verifica se os lados são iguais.
-			
-			
-					
-			this.origemX = origemX;
-			this.origemY = origemY;
 
-			this.largura = origemX + dx;
-			this.altura  = origemY + dy;
+			setDx(dx);
+			setDy(dy);
 		}
 	}
 
-	public void toString(){
-		System.out.println("");
+	//Retorna o valor de Dx.
+	public double getDx(){
+		return this.Dx;
+	}
+
+	//Armazena um valor em Dx.
+	public void setDx(double dx){
+		this.Dx = dx;
+	}
+
+	//Retorna o valor  de Dy.
+	public double getDy(){
+		return this.Dy;
+	}
+
+	//Armazena um valor em Dy.
+	public void setDy(double dy){
+		this.Dy = dy;
+	}
+
+	public void desenha(){
+		System.out.println(toString());
+	}
+
+	public String toString(){
+		return "Quadrado";
 	}
 
 	//Move  quadrado de sua origem.
-	void mover(float dx, float dy){
-		
+	public void mover(float dx, float dy){
+		this.Dx += dx;
+		this.Dy += dy;
 	}
 
 	//Calcula a área do quadrado;
-	int calcularArea(){
-		int area = dx*dy;
+	public double calculaArea(){
+		double area = Dx * Dy;
 
 		return area;
 	}
