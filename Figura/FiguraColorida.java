059dcd705494c35391e706e2d4d307782dcc6c4b
Breno Araújo <31549095+brenoaraujolima@users.noreply.github.com>
2018-03-28 22:37:08 -0300

    Update FiguraColorida.java

--- Figura/FiguraColorida.java
+++ Figura/FiguraColorida.java
@@ -1,3 +1,5 @@
+/* Implementado por : Breno Araújo de Lima. Mat: 398583 e Francisco San Diego de Sousa Castilho.  Mat: 397377.*/
+
 public interface FiguraColorida {
 
 	public void desenhar(String cor);
