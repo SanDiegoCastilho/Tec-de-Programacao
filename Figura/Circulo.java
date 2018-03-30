56935370e00aefa71e4667c350fa362b2352551e
SanDiegoCastilho <sandiegocastilho@gmail.com>
2018-03-29 22:50:39 -0300

    Modificações do circulo

--- Figura/Circulo.java
+++ Figura/Circulo.java
@@ -1,18 +1,15 @@
 //IMPLEMENTADO POR BRENO ARAÚJO DE LIMA - Mat:398583 E FRANCISCO SAN DIEGO DE SOUSA CASTILHO Mat:397377.
 //Classe Círculo - questão 02b Lista 02.
-//Última modificação 29/03/2018 - 22:19; 
+//Última modificação 29/03/2018 - 22:35; 
 
 public class Circulo extends Figura{
 
 	private double Raio; //Raio da circunferência.
-	private double Px; //Representa o valor do x do ponto de origem do círculo.
-	private double Py; //Representa o valor do y do ponto de origem do círculo.   
 
 	//Construtor padrão
 	public Circulo(double px, double py, double raio){
-		setPx(px);
-		setPy(py);
-		setRaio(raio);
+		super(px, py);  //Conrutor da classe abstrata.
+		setRaio(raio); //Armazena o valor do Raio.
 	}
 
 	//Construtor vazio.
@@ -25,6 +22,11 @@ public class Circulo extends Figura{
 		if(raio > 0){
 			this.Raio = raio;
 		}
+	}
+
+	//Retorna o valor do Raio.
+	public double getRaio(){
+		return this.Raio;
 	}
 
 	//Armazendo um valor a Px.
@@ -57,7 +59,7 @@ public class Circulo extends Figura{
 
 	//Método responsável por calcular a área dó círculo.
 	public double calculaArea(){
-		double Area = Math.pow(this.Raio) * 3.14;
+		double Area = Math.pow(getRaio(), 2) * 3.14;
 
 		return Area;
 	}
