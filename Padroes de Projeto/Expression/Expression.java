c51bc779688705d6afdf752ccb0e883c3f946825
SanDiegoCastilho <sandiegocastilho@gmail.com>
2018-04-12 09:05:53 -0300

    lista 05

---
+++ Padroes de Projeto/Expression/Expression.java
@@ -0,0 +1,8 @@
+public abstract class Expression{
+	
+	//Operation do Composite.
+	public abstract int calcular();
+
+	//Add do Composite.
+	public abstract void adicionar(Expression e, Expression d);
+}
